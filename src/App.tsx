de8ba7cf9b6ebf06cff3995547887675234daa5f
ankitbhandari <newmew.ankit@gmail.com>
2024-06-29 12:18:50 +0545

    footer layout added

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import BackgroundComets from "./Components/BackgroundComets";
 import Profile from "./Components/Profile";
 import Blog from "./Pages/Blog";
 import BlogPost from "./Pages/BlogPost";
+import Home from "./Pages/Home";
+import AnimatedSign from "./Components/AnimatedSign";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
 
 const App: React.FC = () => {
   return (
@@ -16,10 +20,7 @@ const App: React.FC = () => {
           <Profile />
           <div className="w-full max-w-4xl px-4 mt-8">
             <Routes>
-              <Route
-                path="/"
-                element={<p className="text-white text-2xl">Home</p>}
-              />
+              <Route path="/" element={<Home />} />
               <Route path="/blog" element={<Blog />} />
               <Route path="/blog/:postHashHex" element={<BlogPost />} />
               <Route
@@ -27,6 +28,22 @@ const App: React.FC = () => {
                 element={<p className="text-white text-2xl">Post</p>}
               />
             </Routes>
+          </div>
+
+          {/* Combined AnimatedSign and text with minimal spacing */}
+          <div className="flex flex-col items-center mt-4 pb-7">
+            <div className="w-48">
+              <AnimatedSign />
+            </div>
+            <div className="flex items-center flex-col">
+              <p className="text-white text-xl -mt-6">
+                I love product designing & AI!
+              </p>
+              <div className="text-white text-xl mt-2 flex gap-6 opacity-75">
+                <FaXTwitter />
+                <FaInstagram />
+              </div>
+            </div>
           </div>
         </div>
       </div>
