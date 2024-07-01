0f9af36d3a36f8a123a96f1a757ccd15ba1ea2a4
ankitbhandari <newmew.ankit@gmail.com>
2024-07-01 20:12:12 +0545

    lens provider issue

---
+++ src/Providers.tsx
@@ -0,0 +1,36 @@
+import {
+  LensConfig,
+  LensProvider,
+  development,
+} from "@lens-protocol/react-web";
+import { bindings } from "@lens-protocol/wagmi";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WagmiProvider, createConfig, http } from "wagmi";
+import { polygon, polygonAmoy } from "wagmi/chains";
+
+const queryClient = new QueryClient();
+
+const wagmiConfig = createConfig({
+  chains: [polygonAmoy, polygon],
+  transports: {
+    [polygonAmoy.id]: http(),
+    [polygon.id]: http(),
+  },
+});
+
+const lensConfig: LensConfig = {
+  environment: development,
+  bindings: bindings(wagmiConfig),
+  debug:
+    typeof window !== "undefined" && window.location.search.includes("debug"), // Check for window
+};
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <LensProvider config={lensConfig}>{children}</LensProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
